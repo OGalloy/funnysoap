8de9a5c2d4aa5e1369aa27b0f1e3c3b5e9e66405
Alex <121458630+Rasofex@users.noreply.github.com>
2023-12-05 23:33:58 +0300

    add: ui + fonts

---
+++ client/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex w-full min-h-screen flex-col">
+    <div class="px-0 md:px-4 lg:px-10 w-full pt-4 flex justify-center">
+      <div class="w-full lg:w-5xl lg:max-w-5xl">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
